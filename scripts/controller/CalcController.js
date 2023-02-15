f08c18604d7c596b9e620bb25097158867f912ff
joao.gimenes.clear <joao.gimenes.clear@gmail.com>
2023-02-15 08:39:06 -0300

    Added Audio when double click in AC button

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController{
 
     constructor(){
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
         this._operation = [];
@@ -59,11 +61,41 @@ class CalcController{
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
 
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+
+            btn.addEventListener('dblclick', e=>{
+
+                this.toggleAudio();
+
+            })
+
+        });
+
+    }
+
+    toggleAudio(){
+
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio(){
+
+        if (this._audioOnOff){
+
+            this._audio.currentTime = 0;
+
+            this._audio.play();
+
+        }
+
     }
 
     initKeyboard(){
 
         document.addEventListener('keyup', e=>{
+
+            this.playAudio();
 
             switch (e.key) {
 
@@ -327,6 +359,8 @@ class CalcController{
     }
 
     execBtn(value){
+
+        this.playAudio();
 
         switch (value) {
 
